d34db8aa283aa551aeeb17f022e7f5e1fe47fa99
akd40 <akd40@pitt.edu>
2016-05-08 13:38:55 -0400

    Push to heroku for debug

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ function postMessage(resNum, word) {
 
     trollface.first(function(json) {
     console.log(json);
-    botResponse = json.definition;
+    botResponse = JSON.stringify(json.definition);
     });
 
   }
